9538471c3093caf7361469b062b21259a4a590bf
vikramsingh <mr.vikram004@gmail.com>
2023-05-01 23:14:12 +0400

    module specific navigator added

--- appModules/hostAvailability/src/main/kotlin/app/modules/hostavailability/di/HostAvailabilityFragmentModule.kt
+++ appModules/hostAvailability/src/main/kotlin/app/modules/hostavailability/di/HostAvailabilityFragmentModule.kt
@@ -2,6 +2,8 @@ package app.modules.hostavailability.di
 
 import androidx.fragment.app.Fragment
 import app.modules.hostavailability.adapter.HostAvailabilityItemCallback
+import app.reprator.navigation.AppNavigator
+import app.reprator.navigation.HostAvailabilityNavigator
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,6 +12,11 @@ import dagger.hilt.android.components.FragmentComponent
 @InstallIn(FragmentComponent::class)
 @Module
 class HostAvailabilityModule {
+
+    @Provides
+    fun provideHostAvailabilityNavigator(
+        appNavigator: AppNavigator
+    ): HostAvailabilityNavigator = appNavigator
 
     @Provides
     fun provideHostAvailabilityItemCallback(
