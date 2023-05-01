7c9a003947f2d20448911a892fb1b62a4384e426
vikramsingh <mr.vikram004@gmail.com>
2023-05-01 22:30:30 +0400

    remote datasource implementation

---
+++ appModules/hostAvailability/src/main/kotlin/app/modules/hostavailability/datasource/remote/HostAvailabilityRemoteDataSourceImpl.kt
@@ -0,0 +1,41 @@
+package app.modules.hostavailability.datasource.remote
+
+import app.modules.hostavailability.data.datasource.HostAvailabilityRemoteDataSource
+import app.modules.hostavailability.datasource.remote.mapper.HostAvailabilityMapper
+import app.modules.hostavailability.modal.ModalHostContainer
+import app.reprator.base.useCases.AppError
+import app.reprator.base.useCases.AppResult
+import app.reprator.base.useCases.AppSuccess
+import app.reprator.base.util.network.safeApiCall
+import app.reprator.base.util.network.toResult
+import javax.inject.Inject
+
+class HostAvailabilityRemoteDataSourceImpl @Inject constructor(
+    private val apiService: HostAvailabilityApiService,
+    private val mapper: HostAvailabilityMapper
+) : HostAvailabilityRemoteDataSource {
+
+    private suspend fun getHostAvailabilityRemoteDataSourceApi():
+            AppResult<ModalHostContainer> {
+
+        return when (val data = apiService.hostAvailabilityResponse().toResult()) {
+            is AppSuccess -> {
+                val result = data.data
+                if(true == result.success)
+                    AppSuccess(mapper.map(result))
+                else
+                    AppError(message = result.message)
+            }
+
+            is AppError -> {
+                AppError(message = data.message, throwable = data.throwable)
+            }
+
+            else -> throw IllegalArgumentException("Illegal State")
+        }
+    }
+
+    override suspend fun getHostAvailabilityRemoteDataSource(): AppResult<ModalHostContainer> =
+        safeApiCall(call = { getHostAvailabilityRemoteDataSourceApi() })
+
+}
